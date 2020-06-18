a780803515604ab2fe58a8cb499dd3cf88092a81
Richard William Seddon III <tsed@ymail.com>
2020-06-17 21:48:17 -0500

    Changing randomtts to rtts

--- GlobalBot.py
+++ GlobalBot.py
@@ -1213,7 +1213,7 @@ commands.append(command('source', 'Sends the link to the bot source code'))
 commands.append(command('getbackup', 'Creates and sends a backup of the server', 'getBackup'))
 commands.append(command('guilds', 'Displays a list of guilds the bot is connected to', admin = True))
 commands.append(command('voicestats', 'Displays voice stats for the specified user. Optionally a user can be specified. Format: !voicestats @user', 'voiceStats'))
-commands.append(command('randomtts', 'Sends a random tts message from the server'))
+commands.append(command('rtts', 'Sends a random tts message from the server', 'randomtts'))
 loadUserCommands()
 
 #launch the refresh timer
